2d3e025ceb84de4c889c2bba3a7a239fc927bb0e
Danyelle <dany.san120404@gmail.com>
2022-08-02 00:06:16 -0300

    Adicionando a função pop-up

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function showModal() {
+    var element = document.getElementById("modal01");
+    element.classList.add("show-modal");
+}
+
+function hideModal(){
+    var element =  document.getElementById("modal01");
+    element.classList.remove("show-modal");
+}
