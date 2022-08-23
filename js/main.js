059d67669fc94715a1a450298afff5240d88f004
Danyelle <dany.san120404@gmail.com>
2022-08-23 20:01:12 -0300

    melhorando a resposividade

--- js/main.js
+++ js/main.js
@@ -1,9 +1,34 @@
+//Função do Pop-up
 function showModal() {
-    var element = document.getElementById("modal01");
+    var element = document.getElementById("modal");
     element.classList.add("show-modal");
 }
 
 function hideModal(){
-    var element =  document.getElementById("modal01");
+    var element =  document.getElementById("modal");
     element.classList.remove("show-modal");
+}
+
+//Função do DropDown
+function myFunction() { 
+  document.getElementById("myDropdown").classList.toggle("show");
+  document.querySelector(".carrossel").style.zIndex = '-1';
+ 
+  
+}
+  // Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event){
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show'),document.querySelector(".carrossel").style.zIndex = '1') {
+        openDropdown.classList.remove('show');
+        
+      }
+      
+    }
+  }
+  
 }
